784f05d583fbe0d66afb45fab22014cb245151bb
minpeter <kali2005611@gmail.com>
2022-03-12 15:52:23 +0900

    changing color & *brush size done.

--- app.js
+++ app.js
@@ -1,27 +1,58 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const ctr = document.getElementById("jsRange");
+
+ctx.lineWidth = 3;
+
+canvas.width = 500;
+canvas.height = 500;
 
 let painting = false;
 
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+function startPainting() {
+  painting = true;
 }
 
 function onMouseDown(event) {
   painting = true;
 }
 
-function onMouseUp(event) {
-  stopPainting();
-}
-
 function stopPainting() {
   painting = false;
 }
 
+function handColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+}
+
+function handRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach((color) =>
+  color.addEventListener("click", handColorClick)
+);
+
+ctr.addEventListener("input", handRangeChange);
